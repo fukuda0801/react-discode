dbe75fb40f37b96ef605591355f1af299536b76d
fukuda0801 <nagisano0801@gmail.com>
2024-08-14 13:31:07 +0900

    front: フロントエンド作成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 import "./Sidebar.scss";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddIcon from '@mui/icons-material/Add';
+import SidebarChannel from "./SidebarChannel";
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
+
 
 function Sidebar() {
   return (
@@ -7,7 +14,7 @@ function Sidebar() {
       {/* sidebarLeft */}
       <div className="sidebarLeft">
         <div className="serverIcon">
-          <img src="/logo192.png" alt="" />
+          <img src="/discordIcon.png" alt="" />
         </div>
         <div className="serverIcon">
           <img src="/logo192.png" alt="" />
@@ -18,6 +25,41 @@ function Sidebar() {
       <div className="sidebarRight">
         <div className="sidebarTop">
           <h3>Discode</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        {/* sidebarChannels */}
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミングチャンネル</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>ハム三郎</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
+          </div>
         </div>
       </div>
     </div>
